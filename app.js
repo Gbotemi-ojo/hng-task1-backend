164480d50b8d5d883b8c592bb840b48148c11699
ojo gbotemi <hommzmum@gmail.com>
2023-09-07 15:33:13 +0100

    completed backend task 1

--- app.js
+++ app.js
@@ -17,18 +17,22 @@ const date = new Date();
 const index = date.getDay();
 
 const current_day = days[index];
+
 const utc = new Date(date.getTime() + date.getTimezoneOffset() * 60000);
-const utc_time = utc.toISOString()
+const utc_time = utc.toISOString();
+
+const github_file_url = "https://github.com/Gbotemi-ojo/hng-task1-backend/blob/main/app.js";
+const github_repo_url = "https://github.com/Gbotemi-ojo/hng-task1-backend"
 app.get('/api',(req,res)=>{
     const slack_name = req.query.slack_name;
     const track = req.query.track
     res.status(200).json({
-        slack_name: slack_name,
-        current_day: current_day,
-        utc_time : utc_time,
-        track: track,
-        github_file_url: "https://github.com/username/repo/blob/main/file_name.ext",
-        github_repo_url: "https://github.com/username/repo",
+        slack_name,
+        current_day,
+        utc_time,
+        track,
+        github_file_url,
+        github_repo_url,
         status_code : res.statusCode
     });
 });
