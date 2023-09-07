3a54380bb701828de724d85ee7e0faae078c85a8
ojo gbotemi <hommzmum@gmail.com>
2023-09-07 20:14:00 +0100

    updated res object

--- app.js
+++ app.js
@@ -11,14 +11,16 @@ const index = date.getDay();
 
 const current_day = days[index];
 
-const utc = new Date(date.getTime() + date.getTimezoneOffset() * 60000);
-const utc_time = utc.toISOString().split('.')[0] + 'Z'
+// const utc = new Date(new Date().getTime() + new Date().getTimezoneOffset() * 60000);
+// const utc_time = utc.toISOString().split('.')[0] + 'Z';
 
 const github_file_url = "https://github.com/Gbotemi-ojo/hng-task1-backend/blob/main/app.js";
 const github_repo_url = "https://github.com/Gbotemi-ojo/hng-task1-backend"
 app.get('/api', (req, res) => {
     const slack_name = req.query.slack_name;
     const track = req.query.track
+    const utc = new Date(new Date().getTime() + new Date().getTimezoneOffset() * 60000);
+    const utc_time = utc.toISOString().split('.')[0] + 'Z';
     res.status(200).json({
         slack_name,
         current_day,
